7eb8b593646f5133f9824c7eb5a78d3945a19696
Jho99999 <jhomadha989@gmail.com>
2026-08-01 12:47:40 +0700

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,13 @@ class AppServiceProvider extends ServiceProvider
     {
         if (app()->isProduction()) {
             URL::forceScheme('https');
+        }
+
+        $target = storage_path('app/public');
+        $link = public_path('storage');
+
+        if (! File::exists($link) && File::exists($target)) {
+            File::link($target, $link);
         }
     }
 }
